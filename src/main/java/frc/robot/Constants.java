8f9c9c045897f962c5cb02cc0e8f0951c00d1a38
Jason SooHoo <jtsoohoo@comcast.net>
2023-10-31 01:19:19 -0700

    added constants file

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,76 @@
+package frc.robot;
+
+/**
+ * @brief Stores all robot specific constants.
+ */
+public final class Constants
+{
+    /**
+     * @brief Contains constants related to hardware.
+     */
+    public static class Hardware
+    {
+
+        /**
+         * @brief The CAN ID of swerve module 1's rotation motor.
+         */
+        public static final int MOTOR_SWERVE_1_ROTATION_ID = 0;
+
+        /**
+         * @brief The CAN ID of swerve module 1's thrust motor.
+         */
+        public static final int MOTOR_SWERVE_1_THRUST_ID = 0;
+
+        /**
+         * @brief The CAN ID of swerve module 1's encoder.
+         */
+        public static final int ENCODER_SWERVE_1 = 0;
+
+        /**
+         * @brief The CAN ID of swerve module 2's rotation motor.
+         */
+        public static final int MOTOR_SWERVE_2_ROTATION_ID = 0;
+
+        /**
+         * @brief The CAN ID of swerve module 2's thrust motor.
+         */
+        public static final int MOTOR_SWERVE_2_THRUST_ID = 0;
+
+        /**
+         * @brief The CAN ID of swerve module 2's encoder.
+         */
+        public static final int ENCODER_SWERVE_2 = 0;
+
+        /**
+         * @brief The CAN ID of swerve module 3's rotation motor.
+         */
+        public static final int MOTOR_SWERVE_3_ROTATION_ID = 0;
+
+        /**
+         * @brief The CAN ID of swerve module 3's thrust motor.
+         */
+        public static final int MOTOR_SWERVE_3_THRUST_ID = 0;
+
+        /**
+         * @brief The CAN ID of swerve module 3's encoder.
+         */
+        public static final int ENCODER_SWERVE_3 = 0;
+
+        /**
+         * @brief The CAN ID of swerve module 4's rotation motor.
+         */
+        public static final int MOTOR_SWERVE_4_ROTATION_ID = 0;
+
+        /**
+         * @brief The CAN ID of swerve module 4's thrust motor.
+         */
+        public static final int MOTOR_SWERVE_4_THRUST_ID = 0;
+
+        /**
+         * @brief The CAN ID of swerve module 4's encoder.
+         */
+        public static final int ENCODER_SWERVE_4 = 0;
+
+    } // END CLASS HARDWARE
+
+} // END CLASS CONSTANTS
